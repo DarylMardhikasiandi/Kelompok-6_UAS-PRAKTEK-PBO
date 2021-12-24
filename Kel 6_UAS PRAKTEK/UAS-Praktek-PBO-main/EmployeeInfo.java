ca02aaff7cec535abe777c90297b45b7c059e21b
DarylMardhikasiandi <92164610+DarylMardhikasiandi@users.noreply.github.com>
2021-12-25 03:11:58 +0700

    Nama Kelompok 6
    Kelompok 6 :
    •AugustaSatrianto(20051214007)
    •Mukhtarul Fata AnNadwi(20051214045)
    •Muhammad Irfan Jazuli(20051214053)
    •Daryl Mardhikasiandi(20051214089)
    •MuhammadFaiz Widya Putra Hardianto(20051214107)

---
+++ Kel 6_UAS PRAKTEK/UAS-Praktek-PBO-main/EmployeeInfo.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Kel 6
+ */
+public interface EmployeeInfo {
+
+    final float FACULTY_MONTHLY_SALARY = 5000.00f; /* constant variables */
+
+    final int STAFF_MONTHLY_HOURS_WORKED = 160;
+
+}
